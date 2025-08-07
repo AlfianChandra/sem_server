6575214f1e1a28d3853bfaf01f30163e538ea653
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-08-07 09:36:55 +0700

    -
    -

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+server.listen(process.env.APP_PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
   bsnwWorker.on("bsnw-update", (data) => {
     console.log(data.value);
   });
